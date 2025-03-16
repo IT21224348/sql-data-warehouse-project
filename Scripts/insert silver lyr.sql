46ba69f6f4434b9b6f0227064d707a78c03e155a
Ravindu Sulakkana <99631317+IT21224348@users.noreply.github.com>
2025-03-17 01:41:04 +0530

    Update insert silver lyr.sql

--- Scripts/insert silver lyr.sql
+++ Scripts/insert silver lyr.sql
@@ -103,3 +103,12 @@ CASE WHEN UPPER(TRIM(gen)) in ('F','FEMALE') THEN 'Female'
 	 ELSE 'N/a'
 END AS gen
 FROM bronze.erp_cust_az_12
+
+INSERT INTO silver.erp_loc_a101(cid,cntry)
+SELECT REPLACE(cid,'-','') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+     WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'N/A'
+	 ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101
