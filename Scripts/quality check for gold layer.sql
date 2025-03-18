7cd35c0606f9e3e15636f42a9b71ff8c56588ac4
Ravindu Sulakkana <99631317+IT21224348@users.noreply.github.com>
2025-03-18 23:35:48 +0530

    Update quality check for gold layer.sql

--- Scripts/quality check for gold layer.sql
+++ Scripts/quality check for gold layer.sql
@@ -1,4 +1,4 @@
---Check for duplicate after joining tables
+--Check for duplicate values after joining table
 SELECT cst_id,COUNT(*) FROM	
 	(SELECT
 		 ci.cst_id,
@@ -18,3 +18,18 @@ SELECT cst_id,COUNT(*) FROM
 	ON ci.cst_key = la.cid
 )t GROUP BY cst_id
 HAVING COUNT(*)> 1
+
+--Integrating gender columns
+
+SELECT DISTINCT
+	ci.cst_gndr,
+	ca.gen,
+    CASE WHEN ci.cst_gndr != 'N/A' THEN cst_gndr     -- CRM table is the master table
+	     ELSE COALESCE(ca.gen,'N/A')
+		 END AS new_gen
+	FROM silver.crm_cust_info ci
+	LEFT JOIN silver.erp_cust_az_12 ca 
+	ON ci.cst_key = ca.cid
+	LEFT JOIN silver.erp_loc_a101 la
+	ON ci.cst_key = la.cid
+ORDER BY 1,2
