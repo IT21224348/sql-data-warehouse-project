c1f74ca9d6582bd8a259b53f8d1d6ba62c30a10c
Ravindu Sulakkana <99631317+IT21224348@users.noreply.github.com>
2025-03-19 17:38:12 +0530

    Update quality check for gold layer.sql

--- Scripts/quality check for gold layer.sql
+++ Scripts/quality check for gold layer.sql
@@ -1,3 +1,4 @@
+====================================================== dim_customer==========================================================
 --Check for duplicate values after joining table
 SELECT cst_id,COUNT(*) FROM	
 	(SELECT
@@ -33,3 +34,35 @@ SELECT DISTINCT
 	LEFT JOIN silver.erp_loc_a101 la
 	ON ci.cst_key = la.cid
 ORDER BY 1,2
+
+
+=======================================================dim_product========================================================================
+--Check for duplicate values after joining table
+SELECT prd_key, COUNT(*) FROM (
+	SELECT 
+		pc.prd_id,
+		pc.prd_key,
+		pc.prd_nm,
+		pc.cat_id,
+		px.cat,
+		px.subcat,
+		px.maintenance,
+		pc.prd_cost,
+		pc.prd_line,
+		pc.prd_start_dt		
+	FROM silver.crm_prd_info pc
+	LEFT JOIN silver.erp_px_cat_g1v2 px
+	ON pc.cat_id = px.id 
+	WHERE pc.prd_end_dt IS NULL) T GROUP BY prd_key -- Only consider current data
+HAVING COUNT(*) > 1
+
+===========================================================fact_sales==================================================
+--Check data integrity (dimensions)
+SELECT *
+FROM gold.fact_sales gs
+LEFT JOIN gold.dim_customers gd
+ON  gs.customer_key = gd.customer_key
+LEFT JOIN gold.dim_products gp
+ON gs.product_key = gp.product_key
+WHERE gs.product_key IS NULL
+--WHERE gs.customer_key IS NULL
